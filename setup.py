2f466b5eb15bf0c107ddf7f977c9b3ac1892c171
tobe <117567068+tobe-core@users.noreply.github.com>
2025-11-13 14:03:39 +0100

    repo reorganization [setup]

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+
+def setup(backup):
+    if backup:
+        os.system("cp -r ~/.config ~/.config.bak")
+
+    os.system("sudo pacman -S brave-bin vesktop obsidian spotify-launcher code nemo swww")
+    
+    os.system("git clone https://github.com/tobe-core/cachyos-hyprconfig.git ~/cachyos-hyprconfig && cp -r ~/cachyos-hyprconfig/* ~/.config/ && rm ~/.config/README.md")
+    
+    if not os.path.exists("~/Documentos/code/PyWall"):
+        os.system("git clone https://github.com/tobe-core/PyWall.git ~/Documentos/code/PyWall && rm ~/Documentos/code/PyWall/README.md")
+
+    os.system("git clone https://github.com/tobe-core/cachyos-hyprconfig-extra.git ~/cachyos-extra && cp -r ~/cachyos-extra/cursors/* ~/.icons/ && cp -r ~/cachyos-extra/wallpapers ~/Im\u00e1genes/")
+
+    os.system("rm -rf ~/cachyos-extra && rm -rf ~/cachyos-hyprconfig")
+
+    exit()
+
+print("Want to do a backup?")
+user = input("(y, n) > ")
+
+if user == "y":
+    setup(True)
+elif user == "n":
+    setup(False)
+else:
+    exit()
